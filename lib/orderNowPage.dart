6e2ee8a0bed611a0042aba5245d53baf4d8ca90c
Shubham nanote <117644977+Shubham02003@users.noreply.github.com>
2022-11-21 11:26:20 +0530

    Update orderNowPage.dart

--- lib/orderNowPage.dart
+++ lib/orderNowPage.dart
@@ -39,114 +39,112 @@ class _OrderNowState extends State<OrderNow> {
                 appBar: AppBar(
                   backgroundColor: Colors.black,
                   title: Text("Product Store"),
-              automaticallyImplyLeading: false,
-              leading: IconButton(
-                icon: const Icon(Icons.arrow_back, color: Colors.white),
-                onPressed: () => Navigator.of(context).pop(),
+                     automaticallyImplyLeading: false,
+                     leading: IconButton(
+                     icon: const Icon(Icons.arrow_back, color: Colors.white),
+                      onPressed: () => Navigator.of(context).pop(),
               ),
             ),
                 body: Padding(
-                  padding: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
-                  child: Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Expanded(
-                          child: Center(
-                            child: Image(
-                              image: NetworkImage(
-                                getApi.getImage(widget.idNumber - 1),
-                              ),
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        child: Center(
+                          child: Image(
+                            image: NetworkImage(
+                              getApi.getImage(widget.idNumber - 1),
                             ),
                           ),
                         ),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: [
-                                  Card(
-                                    color: Colors.green,
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(25),
-                                    ),
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Text(
-                                        '\$${getApi.getPrice(widget.idNumber-1)}',
-                                        style: const TextStyle(
-                                          fontSize: 25,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.white
-                                        ),
+                      ),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Card(
+                                  color: Colors.green,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(25),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text(
+                                      '\$${getApi.getPrice(widget.idNumber-1)}',
+                                      style: const TextStyle(
+                                        fontSize: 25,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white
                                       ),
                                     ),
                                   ),
-                                ],
-                              ),
-                              const Text('Discription: ',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 17,
                                 ),
-                                textAlign: TextAlign.start,
+                              ],
+                            ),
+                            const Text('Discription: ',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 17,
                               ),
-                              Text(
-                                getApi.getDiscription(widget.idNumber - 1),
+                              textAlign: TextAlign.start,
+                            ),
+                            Text(
+                              getApi.getDiscription(widget.idNumber - 1),
+                            ),
+                            const Text('Category: ',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 17,
                               ),
-                              const Text('Category: ',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 17,
-                                ),
-                                textAlign: TextAlign.start,
-                              ),
-                              Text(
-                                getApi.getCategoty(widget.idNumber - 1),
-                              ),
+                              textAlign: TextAlign.start,
+                            ),
+                            Text(
+                              getApi.getCategoty(widget.idNumber - 1),
+                            ),
 
-                                ],
-                          ),
+                              ],
                         ),
-                        Row(
-                          children: [
-                            Expanded(
-                              child: FloatingActionButton.extended(
-                                label: const Text('Wishlist'),
-                                backgroundColor: Colors.grey,
-                                icon: const Icon(
-                                  Icons.favorite,
-                                  size: 24.0,
-                                ),
-                                onPressed: () {},
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: FloatingActionButton.extended(
+                              label: const Text('Wishlist'),
+                              backgroundColor: Colors.grey,
+                              icon: const Icon(
+                                Icons.favorite,
+                                size: 24.0,
                               ),
+                              onPressed: () {},
                             ),
-                            const SizedBox(
-                              width: 40,
-                            ),
-                            Expanded(
-                              child: FloatingActionButton.extended(
-                                label: const Text('Order Now',
-                                style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 17,
-                                  fontWeight: FontWeight.bold
-                                ),),
-                                backgroundColor: Colors.red,
-                                icon: const Icon(
-                                  Icons.check,
-                                  size: 24.0,
-                                  color: Colors.black,
-                                ),
-                                onPressed: () {},
+                          ),
+                          const SizedBox(
+                            width: 40,
+                          ),
+                          Expanded(
+                            child: FloatingActionButton.extended(
+                              label: const Text('Order Now',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 17,
+                                fontWeight: FontWeight.bold
+                              ),),
+                              backgroundColor: Colors.red,
+                              icon: const Icon(
+                                Icons.check,
+                                size: 24.0,
+                                color: Colors.black,
                               ),
+                              onPressed: () {},
                             ),
-                          ],
-                        )
-                      ],
-                    ),
+                          ),
+                        ],
+                      )
+                    ],
                   ),
                 ),
               ),
